8416b656862574c870b44df66f8f57848636bc84
leekool <leejedda@gmail.com>
2023-01-28 22:11:40 +1100

    move bash scripts to .bin (dotfiles)

--- scripts/fvwmscriptpavu.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-focusedwindow=$(xdotool getwindowfocus getwindowname)
-
-if ! [ "$focusedwindow" == "Volume Mixer" ]; then
-    xkill -id $(xwininfo -name "Volume Mixer" | awk 'NR==2{print $4}')
-fi
--- scripts/runterminal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-window=$(xdotool getactivewindow getwindowname)
-
-if [[ "$window" != *"Emacs"* ]]; then
-    alacritty;
-else
-    xdotool key super+space;
-fi
